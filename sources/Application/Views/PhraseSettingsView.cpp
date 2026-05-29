4d327019b298838432c037ed50af97a831d2761a
llozd <llozd@pm.me>
2026-05-29 17:08:23 +0100

    Fix phrase length field

--- sources/Application/Views/PhraseSettingsView.cpp
+++ sources/Application/Views/PhraseSettingsView.cpp
@@ -63,14 +63,34 @@ void PhraseSettingsView::ProcessButtonMask(unsigned short mask, bool pressed) {
     if (mask & EPBM_UP) {
       updateLength(1);
     }
+    if (mask == EPBM_ENTER) {
+      lengthFocused_ = !lengthFocused_;
+      isDirty_ = true;
+    }
+    return;
+  }
+
+  if (lengthFocused_) {
+    if (mask & EPBM_DOWN) {
+      updateLength(-1);
+    }
+    if (mask & EPBM_UP) {
+      updateLength(1);
+    }
     return;
   }
 
   if (mask & EPBM_DOWN) {
-    updateLength(-1);
+    ViewType vt = VT_PHRASE;
+    ViewEvent ve(VET_SWITCH_VIEW, &vt);
+    SetChanged();
+    NotifyObservers(&ve);
   }
   if (mask & EPBM_UP) {
-    updateLength(1);
+    ViewType vt = VT_GROOVE;
+    ViewEvent ve(VET_SWITCH_VIEW, &vt);
+    SetChanged();
+    NotifyObservers(&ve);
   }
 }
 
@@ -92,7 +112,8 @@ void PhraseSettingsView::DrawView() {
   uchar length = viewData_->song_->phrase_.GetLength(viewData_->currentPhrase_);
   char line[SCREEN_WIDTH + 1];
   props.invert_ = lengthFocused_;
-  npf_snprintf(line, sizeof(line), "Length: %2u", static_cast<unsigned>(length));
+  npf_snprintf(line, sizeof(line), "Length: %2u",
+               static_cast<unsigned>(length));
   DrawString(pos._x, pos._y, line, props);
 
   drawMap();
